b47e1224c4d53e50add303e0628ae3006a66331d
AWarmCupOfTea <satussss+1@gmail.com>
2022-07-04 20:55:37 +0200

    Add basic player run

--- scenes/player/base_state.gd
+++ scenes/player/base_state.gd
@@ -5,19 +5,19 @@ onready var _player := find_parent("Player") as Player
 onready var _state_machine := find_parent("StateMachine") as StateMachine
 
 
-func _ready() -> void:
+func _ready():
 	assert(_player is Player)
 	assert(_state_machine is StateMachine)
 
 # BaseState interface implementation.
 
-func _enter() -> void:
+func _enter(_delta: float):
 	pass
 	
-func _exit() -> void:
+func _exit(_delta: float):
 	pass
 
-func _update(_delta: float) -> void:
+func _update(_delta: float):
 	pass
 
 # BaseState interface end.
@@ -25,12 +25,12 @@ func _update(_delta: float) -> void:
 
 
 # Called by StateMachine.
-func physics_process(delta: float) -> void:
+func physics_process(delta: float):
 	if (
 			Input.is_action_just_pressed("jump") 
 			and _player.can_jump() 
 			and _state_machine.current_state != _state_machine.State.JUMP
 	):
-		_state_machine.transition_to(_state_machine.State.JUMP)
+		_state_machine.transition_to(_state_machine.State.JUMP, delta)
 		
 	_update(delta)
