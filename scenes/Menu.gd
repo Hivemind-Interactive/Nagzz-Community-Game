39131202addadd3219c21e3fa9e18477a489d276
ZoazVA <ZmeyFree@gmail.com>
2022-07-07 20:43:35 +0300

    main menu

--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -1,32 +1,11 @@
 extends Control
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+	pass 
 func _on_Start_pressed():
 	get_tree().change_scene("res://scenes/test_scene.tscn")
-
-
 func _on_Options_pressed():
-	pass # Replace with function body.
-
-
+	pass 
 func _on_Credits_pressed():
-	pass # Replace with function body.
-
-
+	pass 
 func _on_Quit_pressed():
 	get_tree().quit()
