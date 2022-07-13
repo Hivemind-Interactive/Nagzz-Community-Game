d2cdea5ab82f35e39f04c1a31f50097465f34ad3
AWarmCupOfTea <satussss+1@gmail.com>
2022-07-13 22:20:10 +0200

    Add variable jump heigh

--- scenes/player/in_air.gd
+++ scenes/player/in_air.gd
@@ -5,17 +5,13 @@ extends BaseState
 var _coyote_time_elapsed := 0.0
 
 
-# BaseState interface implementation.
-
 func _enter():
 	._enter()
-	print("Enter InAir state.")
 	_coyote_time_elapsed = 0.0
 	
 	
 func _exit():
 	._exit()
-	print("Exit InAir state.")
 	
 	
 func _update(delta: float):
@@ -39,4 +35,3 @@ func _update(delta: float):
 	_player.air_move(delta)
 	_player.move_player(delta)
 
-# BaseState interface end.
