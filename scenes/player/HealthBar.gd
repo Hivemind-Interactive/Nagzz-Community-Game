a50ed0c15fe13186feb6b1ef7390e5aa0d4e269e
Mad Hatter <103346535+TheMadHatter01@users.noreply.github.com>
2022-07-20 14:05:44 +1000

    fix the code for player health system

--- scenes/player/HealthBar.gd
+++ scenes/player/HealthBar.gd
@@ -1,14 +1,18 @@
-extends Control
+extends ProgressBar
+
 
 func _ready():
-	self.max_value = HealthStats.max_health
-	self.value = HealthStats.health
+	max_value = HealthStats.max_health
+	value = HealthStats.health
 	HealthStats.connect("max_health_changed", self, "set_max_health_bar")
 	HealthStats.connect("health_changed", self, "set_health_bar")
-	
+
+
 func set_max_health_bar(value):
-	self.max_value = HealthStats.max_health
-	
+	self.max_value = value
+
+
 func set_health_bar(value):
-	self.value = HealthStats.health
-	
+	self.value = value
+
+
--- scenes/player/HitBox.gd
+++ scenes/player/HitBox.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
-onready var player_stats = HealthStats
 
 func _on_HitBox_area_entered(area):
-	player_stats.health -= area.damage
-	
+	HealthStats.health -= area.damage
+
